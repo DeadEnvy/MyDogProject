47ddc463fc253fc8acc13ded7315383f0f5fd654
Marcus Mamott <Marcus.Mamott@gmail.com>
2018-10-24 15:59:45 +0000

    Created loop through JSON data to read dog breed types.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'net/http'
+require 'json'
+require 'pp'
+ 
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+
+info =  JSON.parse(response)
+
+info["message"].each do |breeds|
+   dogBreed = "#{breeds[0]}"
+   dogBreedCaps = dogBreed.capitalize
+ 
+  if breeds[1].count == 0
+  puts "No sub: #{dogBreedCaps}"
+   # main_breed.create(MainBreedName: dogBreedCaps)
+  else 
+    puts "Has sub: #{dogBreedCaps}"
+  end
+
+  breeds[1].each do |subbreeds|
+  subBreedCaps = subbreeds.capitalize
+
+    puts "Sub: #{subBreedCaps}"
+  # sub_breed.create(SubBreedName: subBreedCaps)
+   
+  # fullBreed =  "#{subBreedCaps} #{dogBreedCaps}"
+
+
+  end
+end
+
