a1fdc49e43a0562659f5fe23a843375bf1b08c34
Marcus <Marcus.mamott@gmail.com>
2018-10-26 13:28:39 -0500

    Changed my views to use the Dog table instead of MainBreed. Coded my show view.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,12 @@
 class DogsController < ApplicationController
   def index
     #@dogs = MainBreed.all
-
-    @dogs = MainBreed.order('MainBreedName ASC').page(params[:page]).per(25)
+    @dogs = Dog.order('main_breed_id ASC').page(params[:page]).per(25)
+    #@dogs = MainBreed.order('MainBreedName ASC').page(params[:page]).per(25)
+    
   end
 
   def show
+   @dog = Dog.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'dogs', to: 'dogs#index'
+resources :dogs, only: [:index, :show]
+
+
   root to: 'dogs#index'
 
-  get 'dogs/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
