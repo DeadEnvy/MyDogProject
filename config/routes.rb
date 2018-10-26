1b2e190097fb4d046324c1ba44175f7d22ee0427
Marcus <Marcus.mamott@gmail.com>
2018-10-26 18:39:31 -0500

    Added navigation back to the home page. Also created a controller/view for the About page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'about_us/index'
 resources :dogs, only: [:index, :show]
 
 
   root to: 'dogs#index'
 
+  get "/home", to: "dogs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
