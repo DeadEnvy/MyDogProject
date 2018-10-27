281207f71ed2826de893ad31557b4a2cb8cbef8a
Marcus Mamott <Marcus.Mamott@gmail.com>
2018-10-27 00:24:16 +0000

    Created an About Us controller and view and created navigation to it from my nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'about_us/index'
 resources :dogs, only: [:index, :show]
 
 
   root to: 'dogs#index'
 
   get "/home", to: "dogs#index"
+  get "/about", to: "about_us#index", as: "index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
